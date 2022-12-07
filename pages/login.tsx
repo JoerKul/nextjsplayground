d7f446c96f6e62e76c684c8dc7f364caf1495bcb
Osvald <osvald@gmx.de>
2022-12-07 12:31:29 +0100

    add layout and assets

---
+++ pages/login.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+import Layout from "../layout/layout";
+
+function login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Login</h1>
+    </Layout>
+  );
+}
+
+export default login;
